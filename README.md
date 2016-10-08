dbd498a8d0ef60ba9bc4bdba511c25fb9bc9e2d6
Avin Grape <carcinogen75@gmail.com>
2016-10-08 23:29:26 +0300

    check file mod time

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ Tile cache server with gray-scale image converting ability.
 ## Usage
 
 ```
-    http://localhost:8080/?x=310&y=158&z=9&server=yandex&gs=1
+http://SERVER-HOST:PORT/?x=310&y=158&z=9&server=yandex&gs=1
 ```
--- controllers/default.go
+++ controllers/default.go
@@ -19,22 +19,31 @@ type TileController struct {
 
 func GrayScale(filename string) string {
 
+	//read file
 	infile, err := os.Open(filename)
 	if err != nil {
 		panic(err.Error())
 	}
 	defer infile.Close()
 
-	// Must specifically use jpeg.Decode() or it
-	// would encounter unknown format error
+	//decode PNG
 	src, err := png.Decode(infile)
 	if err != nil {
 		panic(err.Error())
 	}
 
+	//convert
 	gray := grayscale.Convert(src, grayscale.ToGrayLuminance)
 
+	//get name for converted file
 	outFileName := filename[:len(filename) - 4] + ".gs.png"
+
+	if _, err := os.Stat(outFileName); err == nil {
+		//if file exist - return it now
+		return outFileName
+	}
+
+	//save converted file
 	outfile, err := os.Create(outFileName)
 	if err != nil {
 		panic(err.Error())
@@ -45,12 +54,8 @@ func GrayScale(filename string) string {
 	return outFileName
 }
 
-func DownloadTile(url string, x string, y string, z string, server string, gs bool) (string, error) {
-	//make path
-	path := filepath.Join("cache", server, x, y)
-	os.MkdirAll(path, os.ModePerm)
-	fileName := filepath.Join(path, z + ".png")
-
+//download file
+func DownloadFile(fileName string, url string) {
 	//create output file
 	out, err := os.Create(fileName)
 	if err != nil {
@@ -67,6 +72,25 @@ func DownloadTile(url string, x string, y string, z string, server string, gs bo
 
 	//write content to destination file
 	io.Copy(out, resp.Body)
+}
+
+//get file from cache or download (and convert to GS)
+func GetTile(url string, x string, y string, z string, server string, gs bool, ttl time.Duration) (string, error) {
+
+	//make path
+	path := filepath.Join("cache", server, x, y)
+	os.MkdirAll(path, os.ModePerm)
+	fileName := filepath.Join(path, z + ".png")
+
+	if file, err := os.Stat(fileName); err != nil {
+		//if file not exist
+		DownloadFile(fileName, url)
+	} else {
+		//if file older then ttl duration
+		if (time.Now().Add(-ttl).After(file.ModTime())) {
+			DownloadFile(fileName, url)
+		}
+	}
 
 	//if we gonna get grayScale tile - convert it
 	if (gs) {
@@ -80,13 +104,12 @@ func DownloadTile(url string, x string, y string, z string, server string, gs bo
 
 func (c *TileController) Get() {
 
-
 	var ttlSeconds int = 86400 //cache timeout in seconds
 
 	var ttl time.Duration = time.Duration(time.Duration(ttlSeconds) * time.Second); //cache timeout in seconds
-	var x int = c.GetString("x");
-	var y int = c.GetString("y");
-	var z int = c.GetString("z");
+	var x string = c.GetString("x");
+	var y string = c.GetString("y");
+	var z string = c.GetString("z");
 	var gs bool = false; //is grayScale
 	gs, _ = strconv.ParseBool(c.GetString("gs"));
 
@@ -116,11 +139,15 @@ func (c *TileController) Get() {
 		c.StopRun()
 	}
 
-	fileName, _ := DownloadTile(url, x, y, z, server, gs);
+	//get tile file
+	fileName, _ := GetTile(url, x, y, z, server, gs, ttl);
 
+	//Setup headers
 	c.Ctx.Output.Header("Expires", time.Now().Add(ttl).Format("Sat, 15 Oct 2016 19:31:22 GMT"))
 	c.Ctx.Output.Header("Cache-Control", "public, max-age=" + strconv.Itoa(ttlSeconds * 60));
 	c.Ctx.Output.Header("Content-Type", "image/png")
 	c.Ctx.Output.Header("Content-Transfer-Encoding", "binary")
+
+	//response file
 	http.ServeFile(c.Ctx.Output.Context.ResponseWriter, c.Ctx.Output.Context.Request, fileName)
 }
